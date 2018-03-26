1bc15abb96c95979637c6c35f9d7a6a97649bdc7
ksm1 <sun5289@naver.com>
2018-03-26 10:43:35 +0900

    두번째

--- class1-1.py
+++ class1-1.py
@@ -3,8 +3,7 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 import os
-
-
+print("111")
 class UserInfo:
     def set_info(self, name,phone):
         self.name = name
